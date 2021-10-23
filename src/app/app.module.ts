412cda4d1a70d6a470dee905e58765c9a958ea8f
Fabio Moreno <linkiez@gmail.com>
2021-10-23 16:09:06 -0300

    Added nav-bar and home

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,25 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { NavBarComponent } from './nav-bar/nav-bar.component';
+import { HomeComponent } from './home/home.component';
+import { AppRoutingModule } from './app-routing.module';
+
+import {MenuModule} from 'primeng/menu';
+import {MenubarModule} from 'primeng/menubar';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavBarComponent,
+    HomeComponent,
+
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    AppRoutingModule,
+    MenuModule,
+    MenubarModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/nav-bar/nav-bar.component.ts
@@ -0,0 +1,23 @@
+import { Component, OnInit } from '@angular/core';
+import { MenuItem } from 'primeng/api';
+
+@Component({
+  selector: 'nav-bar',
+  templateUrl: './nav-bar.component.html',
+  styleUrls: ['./nav-bar.component.css'],
+})
+export class NavBarComponent implements OnInit {
+  items: MenuItem[] = [];
+
+  constructor() {}
+
+  ngOnInit() {
+    this.items = [
+      {
+        label: 'Cadastro',
+        items: [{ label: 'Produtos' }, { label: 'Fonecedores' }],
+      },
+      { label: 'Vendas' },{label: "Compras", items: [{label:'Pedido de Compras'}]}
+    ];
+  }
+}
